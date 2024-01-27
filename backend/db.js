e3ccccff0120d124cc1ec0a68e2d69fc198b7f15
nik132-eng <nikunjrohit10@gmail.com>
2024-01-27 11:45:33 +0530

    BD- add account schema

--- backend/db.js
+++ backend/db.js
@@ -31,9 +31,24 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// Create a Schema for account
+const accountSchema = new mongoose.Schema({
+  userId: {
+      type: mongoose.Schema.Types.ObjectId, // Reference to User model
+      ref: 'User',
+      required: true
+  },
+  balance: {
+      type: Number,
+      required: true
+  }
+});
+
 // Create a model from the schema
 const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
 
 module.exports = {
-	User
+	User,
+  Account
 };

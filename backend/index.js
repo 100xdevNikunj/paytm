34db44ea9949e1772e777cf73c128d1ae377f362
nik132-eng <nikunjrohit10@gmail.com>
2024-01-27 11:44:19 +0530

    add dotenv file

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const rootRouter = require("./routes/index");
 var cors = require('cors')
+require('dotenv').config()
 
 const port = process.env.PORT;
 
@@ -10,6 +11,10 @@ app.use(express.json());
 app.use(cors())
 
 app.use("/api/v1", rootRouter);
+
+app.get("/",(req, res)=>{
+    res.send("HELLOOOOOO")
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);

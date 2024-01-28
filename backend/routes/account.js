5ea751bdce0c37171457bcb3c532b56b60d1fb03
nik132-eng <nikunjrohit10@gmail.com>
2024-01-28 23:56:36 +0530

    BD- add transfer logic

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const { User, Account } = require('../db');
 const { authMiddleware } = require('../middleware');
@@ -13,8 +14,51 @@ router.get("/balance", authMiddleware, async(req, res)=>{
     })
 })
 
-router.get("/",(req, res)=>{
-res.send("HEKKKK")
-})
+router.post("/transfer", authMiddleware, async (req, res) => {
+    try {
+        const amount = req.body.amount;
+        const toAddress = req.body.to;
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
+        const account = await Account.findOne({
+            userId: req.userId
+        }).session(session);
+
+        if (!account || account.balance < amount) {
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(400).json({
+                message: "Insufficient balance"
+            });
+        }
+
+        const toAccount = await Account.findOne({ userId: toAddress }).session(session);
+
+        if (!toAccount) {
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(400).json({
+                message: "Invalid account"
+            });
+        }
+
+        // Perform the transfer
+        await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
+        await Account.updateOne({ userId: toAddress }, { $inc: { balance: amount } }).session(session);
+
+        // Commit the transaction
+        await session.commitTransaction();
+        session.endSession();
+        res.json({
+            message: "Transfer successful"
+        });
+    } catch (error) {
+        console.error("Error in transfer transaction:", error);
+        return res.status(500).json({
+            message: "An error occurred during the transfer"
+        });
+    }
+});
 
 module.exports = router;
